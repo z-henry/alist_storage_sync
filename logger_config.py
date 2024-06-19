c67e3918601d32e2591ffd0f058eddd2b91f001d
henryz <henryz@123.com>
2024-06-19 10:39:48 +0800

    支持标准的docker日志

--- logger_config.py
+++ logger_config.py
@@ -4,11 +4,18 @@ from logging.handlers import RotatingFileHandler
 # 配置日志记录
 log_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
-log_handler = RotatingFileHandler('app.log', maxBytes=10000, backupCount=1)
-log_handler.setFormatter(log_formatter)
-log_handler.setLevel(logging.INFO)
+# 文件处理器
+file_handler = RotatingFileHandler('app.log', maxBytes=10000, backupCount=1)
+file_handler.setFormatter(log_formatter)
+file_handler.setLevel(logging.INFO)
+
+# 控制台处理器
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setFormatter(log_formatter)
+console_handler.setLevel(logging.INFO)
 
 # 获取logger实例并配置
 logger = logging.getLogger()
-logger.addHandler(log_handler)
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
 logger.setLevel(logging.INFO)
